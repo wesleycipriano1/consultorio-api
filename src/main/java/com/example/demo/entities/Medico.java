5b48be9d5adbfa3d238acfce1aa63baa02d7807b
wesleycipriano1 <wesleycipriano1@gmail.com>
2023-05-11 13:06:28 -0300

    repository inicial

---
+++ src/main/java/com/example/demo/entities/Medico.java
@@ -0,0 +1,82 @@
+package com.example.demo.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
+public class Medico implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private long crm;
+	private String nome;
+	private String especialidade;
+
+	public Medico() {
+
+	}
+
+	public Medico(long id, long crm, String nome, String especialidade) {
+		super();
+		this.id = id;
+		this.crm = crm;
+		this.nome = nome;
+		this.especialidade = especialidade;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getCrm() {
+		return crm;
+	}
+
+	public void setCrm(long crm) {
+		this.crm = crm;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Medico other = (Medico) obj;
+		return id == other.id;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEspecialidade() {
+		return especialidade;
+	}
+
+	public void setEspecialidade(String especialidade) {
+		this.especialidade = especialidade;
+	}
+
+}
